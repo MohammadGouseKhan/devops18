21656e66ce37534542bafa638a94d9d3f64ce467
MohammadGouseKhan <65332485+MohammadGouseKhan@users.noreply.github.com>
2024-01-29 15:59:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,6 @@ resource "aws_launch_configuration" "web_server_as" {
 }
    
 
-
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-06bf36ef35554f526", "subnet-04215682caa464cc3"]
-     listener {
-      instance_port     = 8000
-      instance_protocol = "http"
-      lb_port           = 80
-      lb_protocol       = "http"
-    }
-    tags = {
-      Name = "terraform-elb"
-    }
-  }
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
     launch_configuration = aws_launch_configuration.web_server_as.name
@@ -27,7 +12,6 @@ resource "aws_autoscaling_group" "web_server_asg" {
     max_size             = 3
     desired_capacity     = 2
     health_check_type    = "EC2"
-    load_balancers       = [aws_elb.web_server_lb.name]
     availability_zones    = ["us-east-1a", "us-east-1b"] 
     
   }
